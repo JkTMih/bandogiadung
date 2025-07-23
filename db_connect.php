9985b38d4ce435014ce7dd80909aba2592e01314
JkTMih <tri7a1@gmail.com>
2025-07-23 21:59:08 +0700

    db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,7 @@ $user = "root";
 $pass = "";
 $dbname = "household_store";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$connection = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
